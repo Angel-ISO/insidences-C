08901a8a4443db52b9f2b9c1d77cb9ff895c6476
Angel-ISO <angelgabrielorteg@gmail.com>
2023-08-17 18:01:28 -0500

    feat(web api base): :construction: repo generico. iterfaces
    se generaron los repositorios y interfaces
    
    base para las demas interfaces y repositorios

--- Aplicacion/Repository/RolRepository.cs
+++ Aplicacion/Repository/RolRepository.cs
@@ -2,16 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using system.Linq.Expressions;
 using Dominio;
 using Dominio.Interfaces;
 using Persistencia;
 
 namespace Aplicacion.Repository;
-
 public class RolRepository : GenericRepository<Rol>, IRolRepository
 {
-    public RolRepository (IncidenceContext context) : base(context)
+    public RolRepository(IncidenceContext context) : base(context)
     {
     }
 }
