7d74659ae8c4306266a0da997a7b3a766bd0e809
Angel-ISO <angelgabrielorteg@gmail.com>
2023-08-29 09:25:20 -0500

    🔥 jwt añadido

--- Aplicacion/Repository/UserRepository.cs
+++ Aplicacion/Repository/UserRepository.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Dominio;
 using Dominio.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Persistencia;
 
 namespace Aplicacion.Repository;
 public class UserRepository : GenericRepository<User>, IUserRepository
 {
+    private readonly IncidenceContext _context;
     public UserRepository(IncidenceContext context) : base(context)
     {
-        
+        _context = context;
+    }
+    public async Task<User> GetByUserNameAsync (string userName)
+    {
+        return await _context.Users.Include(u => u.Rols).FirstOrDefaultAsync (u => u.Name_User.ToLower()==userName.ToLower());
     }
 }
